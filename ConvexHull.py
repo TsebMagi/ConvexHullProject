9e401c6d82e4b10b5937573ee3f8468e2e07c8f2
Doug Whitley <dwhitley@pdx.edu>
2016-11-23 21:02:37 -0800

    several algorithms implemented

--- ConvexHull.py
+++ ConvexHull.py
@@ -2,18 +2,29 @@
 # Fall 2016
 # Convex Hull Testing Program
 
+import MonotoneChain
 import QuickHull
 import random
+import time
+import OtherQuickHull
 
+random.seed(time.clock())
 points = []
-points_2 = [(0,0), (0,1), (2, 0), (1,1)]
+points_2 = [(0, 0), (0, 1), (1, 0), (1, 1)]
 if __name__ == '__main__':
-    #open file for storing results
-    #run testing 10 times on 10 random inputs
-    for num in range(0, 10):
-        for num in range(0,10):
-            points.append((num-1,num+1))
+    # open file for storing results
+    # run testing 10 times on 10 random inputs
+    for num in range(0, 1000):
+        points.append((random.randint(0 , 100), random.randint(0 , 100)))
 
+    print(points)
+    q_points = points
+    m_points = points
+    q_points = OtherQuickHull.get_hull_points(q_points)
+    m_points = MonotoneChain.convex_hull(m_points)
 
-print(points_2)
-print(points)
+    print ("Outputs!")
+    print("Quick Hull: ")
+    print(q_points)
+    print("Monotone Chain: ")
+    print(m_points)
