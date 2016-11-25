f97114fa461009d782263ed7cfe35d12158d6ea3
TsebMagi <dwhitley@pdx.edu>
2016-11-25 12:25:13 -0800

    Run 2 of the algorithms

--- ConvexHull.py
+++ ConvexHull.py
@@ -13,9 +13,9 @@ points = []
 
 if __name__ == '__main__':
     # open file for storing results
-    output = open('testingOutput.txt','w')
+    output = open('TestingOutputRun2.txt','w')
     for num in range(1, 31):
-
+        print("Run ", num)
         for i in range(0, num* 10000):
             points.append((random.randint(-100000, 100000), random.randint(-100000 , 100000)))
 
@@ -29,26 +29,26 @@ if __name__ == '__main__':
 
         # Time the Quick Hull
         start_time = time.time() * 1000
-        q_points = QuickHull.quick_hull(q_points)
+        QuickHull.quick_hull(q_points)
         end_time = time.time() * 1000
-        q_time = end_time - start_time
+        q_time += end_time - start_time
 
         # Time the Quick Hull
         start_time = time.time() * 1000
-        m_points = MonotoneChain.convex_hull(m_points)
+        MonotoneChain.convex_hull(m_points)
         end_time = time.time() * 1000
         m_time = end_time - start_time
 
         # Time the Quick Hull
         start_time = time.time() * 1000
-        g_points = GrahamsScan.convex_hull(g_points)
+        GrahamsScan.convex_hull(g_points)
         end_time = time.time() * 1000
         g_time = end_time - start_time
 
-        header_string = "Outputs for iteration: " + num.__str__() + " with input size "+ (num*10000).__str__() +"\n"
-        QuickHull_string = "Quick Hull: "+ q_time.__str__() + " Milliseconds\n"
-        MonotoneChain_string = "Monotone Chain: " + m_time.__str__() + " Milliseconds\n"
-        GrahamsScan_string = "Graham's Scan: " + g_time.__str__() + " Milliseconds\n"
+        header_string = "input size "+ (num*10000).__str__() +"\n"
+        QuickHull_string = "Quick Hull "+ q_time.__str__() +"\n"
+        MonotoneChain_string = "Monotone Chain " + m_time.__str__() + "\n"
+        GrahamsScan_string = "Graham's Scan " + g_time.__str__() + "\n"
         output.write(header_string)
         output.write(QuickHull_string)
         output.write(MonotoneChain_string)
